57b54bee76eeb107e2c9e27a70f59db757413a1e
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-02-10 16:11:14 +0530

    Add files via upload

---
+++ Ep-1.js
@@ -0,0 +1,20 @@
+// How javascript works?
+// Is js synchronous or asyncronous?
+// is js single threaded or multi threaded?
+
+// ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
+// Everything in Js happens inside an "Execution Context"
+// We can assume this execution context (ec) to be a big
+// box or a container in which whole js code is executed.
+// ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
+
+// EC is like a big box.
+// and it has 2 components in it
+// see the img attached
+
+// JavaScript is a synchronous "single-threaded language"
+// means when i say single threaded which means js can only 
+// execute one line at a time. now synchronous single threaded 
+// means js can only execute one command at a time and in a 
+// specific order, that means it can only go to the enxt line
+// once the current line has been finished executing.
